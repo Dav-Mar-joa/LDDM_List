757271a42fdd4d0966922f99c0c2e44293c85a48
David MARTINS <david.joaquim.martins@gmail.com>
2026-03-21 05:25:34 +0100

    awake

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 
 const { MongoClient, ObjectId } = require('mongodb');
 
