b35a93054b8d5221fdb2b31786f0ac7e5938e63b
David MARTINS <david.joaquim.martins@gmail.com>
2026-03-23 05:50:17 +0100

    Ajout web push notifications2

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
-                '/',
-                '/index.html',
+
                 '/assets/css/styles.css',
                 '/assets/icons/icon192.png',
                 '/assets/icons/icon512.png'
